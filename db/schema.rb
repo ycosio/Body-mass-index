3124eb60cbff1a6f92152584907cc086d0696f19
Yair <ycosio@ucol.mx>
2017-03-28 12:21:02 -0600

    Heroku implemented

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170325214907) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "body_masses", force: :cascade do |t|
     t.integer  "user_id"
     t.float    "mass"
@@ -35,8 +38,8 @@ ActiveRecord::Schema.define(version: 20170325214907) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
