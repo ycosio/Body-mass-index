3a4c9bbe2778ed9ab145151ffbc35815afa2453b
Yair <ycosio@ucol.mx>
2017-03-29 15:44:57 -0600

    LocalStore function refactored

--- app/assets/javascripts/general_scripts.js
+++ app/assets/javascripts/general_scripts.js
@@ -1,15 +1,16 @@
 (function(forms){
-
   forms.forEach(function(form){
     Array.from(form.elements).forEach(function(element, index){
-        element.addEventListener('keyup', function(event){localStorage.setItem(element.name,  element.value)});
-        element.addEventListener('change', function(event){localStorage.setItem(element.name, function(){
-          if(element.type == 'checkbox')
-            return element.checked
-          else
-            return element.value
-        }(element))});
+      addMultipleEventListener(element,['keyup','change']);
     });
   });
-
 })(Array.from(document.forms));
+
+function addMultipleEventListener(element,events){
+  events.forEach(function(events){
+    element.addEventListener(events, function(event){
+      let value = element.type == 'checkbox' ? element.checked : element.value
+      localStorage.setItem(element.name, value);
+    });
+  });
+}
